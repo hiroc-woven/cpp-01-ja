ef71db3d31bfccf65ab19afd201b6b14fb186a5d
hiroc-woven <hiroya.chiba@woven-planet.global>
2023-12-15 13:34:55 +0900

    add 05 main.cpp

---
+++ 02_statements_functions_and_errors/05_challenge/main.cpp
@@ -0,0 +1,22 @@
+//# チャレンジ5
+
+//ユーザーが入力した3つの数値の平均を計算するプログラムがあります。さまざまな値を入力してこのプログラムをテストしてください。プログラムは適切に動作しますか。適切に動作しない場合は、想定どおりの出力になるようにプログラムを修正してください。
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    int num1;
+    int num2;
+    int num3;
+    float average;
+
+    std::cout << "Enter three numbers: ";
+    std::cin >> num1 >> num2 >> num3;
+
+    average = static_cast<float> (num1 + num2 + num3) / 3;
+
+    std::cout << "Average is: " << average << std::endl;
+
+    return 0;
+}
